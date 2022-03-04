ef2211ee6ab17522042ac7aaba0e20bf36892a1e
UlianaOr <Ouliana9@gmail.com>
2022-03-04 16:07:21 +0100

    zxcx

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 let canvas = document.getElementById("canvas");
+let ctx = canvas.getContex("2d");
+
+function draw() {
+
+    ctx.fillStyle = "#222";
+    ctx.fillRect(0,0,canvas.width, canvas.height);
+}
